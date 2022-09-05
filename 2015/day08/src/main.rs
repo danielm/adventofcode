ec238020eabb942c205ef275dfd8dd495eeaf376
Alvaro Daniel Morales <daniel85mg@gmail.com>
2022-09-05 19:56:35 -0300

    2015 - day8 - part 2

--- 2015/day08/src/main.rs
+++ 2015/day08/src/main.rs
@@ -8,25 +8,35 @@ fn main() -> io::Result<()> {
 
     let mut total_orig = 0;
     let mut total_escaped = 0;
+    let mut total_encoded = 0;
 
     for row in reader.lines() {
-        let (len, escaped) = str_lengths(row.unwrap().as_str());
+        let (len, escaped, encoded) = str_lengths(row.unwrap().as_str());
 
         total_orig += len;
         total_escaped += escaped;
+
+        total_encoded += encoded;
     }
 
     println!("P1> Number: {}", total_orig - total_escaped);
+    println!("P2> Number: {}", total_encoded - total_orig);
 
     Ok(())
 }
 
-fn str_lengths(input: &str) -> (usize, usize) {
+fn str_lengths(input: &str) -> (usize, usize, usize) {
     let literal = String::from(input.trim());
 
     let escaped = unescape(literal.as_str()).unwrap();
 
-    (literal.chars().count(), escaped.chars().count()-2)// The -2 is from the wrapping quotes
+    let encoded_length = literal.chars().map(|c| match c {
+       '\\'  => 2,
+       '\"' => 2,
+       _ => 1
+    }).sum::<usize>();
+
+    (literal.chars().count(), escaped.chars().count()-2, encoded_length + 2)// The +/- 22 is from the wrapping quotes
 }
 
 ///
@@ -41,29 +51,56 @@ mod tests {
     //
     #[test]
     fn case_1() {
-        let (r1, r2) = str_lengths("\"\"");
+        let (r1, r2, _) = str_lengths("\"\"");
         assert_eq!(r1, 2);
         assert_eq!(r2, 0);
     }
 
     #[test]
     fn case_2() {
-        let (r1, r2) = str_lengths("\"abc\"");
+        let (r1, r2, _) = str_lengths("\"abc\"");
         assert_eq!(r1, 5);
         assert_eq!(r2, 3);
     }
 
     #[test]
     fn case_3() {
-        let (r1, r2) = str_lengths("\"aaa\\\"aaa\"");
+        let (r1, r2, _) = str_lengths("\"aaa\\\"aaa\"");
         assert_eq!(r1, 10);
         assert_eq!(r2, 7);
     }
 
     #[test]
     fn case_4() {
-        let (r1, r2) = str_lengths("\"\\x27\"");
+        let (r1, r2, _) = str_lengths("\"\\x27\"");
         assert_eq!(r1, 6);
         assert_eq!(r2, 1);
+    }
+
+    //
+    // Part 2
+    //
+    #[test]
+    fn case_5() {
+        let (_, _, r) = str_lengths("\"\"");
+        assert_eq!(r, 6);
+    }
+
+    #[test]
+    fn case_6() {
+        let (_, _, r) = str_lengths("\"abc\"");
+        assert_eq!(r, 9);
+    }
+
+    #[test]
+    fn case_7() {
+        let (_, _, r) = str_lengths("\"aaa\\\"aaa\"");
+        assert_eq!(r, 16);
+    }
+
+    #[test]
+    fn case_8() {
+        let (_, _, r) = str_lengths("\"\\x27\"");
+        assert_eq!(r, 11);
     }
 }
